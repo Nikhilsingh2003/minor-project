4bbb029a864fca9453fde12cef7933d760ba5c7a
ankitraz <109873375+ankitraz@users.noreply.github.com>
2023-02-26 13:57:36 +0530

    removed dockerlogin in deploy()

--- script.groovy
+++ script.groovy
@@ -18,12 +18,14 @@ def Deploy(){
     echo "Deploying the application..."
     def makedir = "mkdir /root/new"  // creating a new directory on remote server
     def cd = "cd /root/new"  // changing directory to new directory on remote server where we copied docker-compose.yml file
+    def dockerlogin = "docker login -u ${env.DOCKER_USER} -p ${env.DOCKER_PASS}"  // login to dockerhub on remote server
     def dockerCmd = "TAG=${env.TAG} REPO_NAME=${env.REPO_NAME} docker-compose up -d" // running docker-compose up command on remote server using TAG variable and REPO_NAME variable
+    
     
     sshagent(['github-ssh-key']) {  // using  private ssh key of jenkins to connect to remote production server
         //copying docker-compose.yml file to remote server
         sh "scp docker-compose.yml ${env.SERVER_USER}@${env.SERVER_IP}:/root/new"  // copy docker-compose.yml file to remote server
-        sh "ssh -o StrictHostKeyChecking=no ${env.SERVER_USER}@${env.SERVER_IP} '${makedir};${cd};${dockerCmd}'"  // running commands on remote server, you can add more commands here seperating them with semicolon
+        sh "ssh -o StrictHostKeyChecking=no ${env.SERVER_USER}@${env.SERVER_IP} '${makedir};${cd};${dockerlogin};${dockerCmd}'"  // running commands on remote server, you can add more commands here seperating them with semicolon
         
     }
 }
